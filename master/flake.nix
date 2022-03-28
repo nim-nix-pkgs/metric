8769572e09aa63f2662101a2aecd3e0a39275fa4
hugosenari <hugosenari@gmail.com>
2022-03-27 21:35:05 -0300

    chore: re index metric

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-metric-master.ref   = "refs/heads/master";
   inputs.src-metric-master.owner = "mjendrusch";
   inputs.src-metric-master.repo  = "metric";
+  inputs.src-metric-master.dir   = "";
   inputs.src-metric-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
