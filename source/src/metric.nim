4f268fc789e5819877966abf353c5313a065c75b
hugosenari <hugosenari@gmail.com>
2022-09-03 19:49:41 -0300

    feat(nimpkgs): update lock

--- source/src/metric.nim
+++
@@ -1,9 +0,0 @@
-#
-# metric - bringing the SI to Nim.
-#
-# (c) Copyright 2018 Michael Jendrusch. All rights reserved.
-# This library is licensed under the MIT license.
-# For more information see LICENSE.
-
-import metric / [dimension, unit, siunits, stddim]
-export dimension, unit, siunits, stddim
